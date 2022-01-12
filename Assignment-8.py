7baf1c8fd3433b2521200f68e61a6ed581b92aa6
smohsin4 <66784998+smohsin4@users.noreply.github.com>
2022-01-12 15:03:27 +0530

    Update Assignment-8.py

--- Assignment-8.py
+++ Assignment-8.py
@@ -1,31 +1,39 @@
 """Program to calculate frequency of a given word in a string"""
 
 
-def getFrequency(str, word):
+def getFrequency(str):
     """Takes in a string and a word and returns the frequency of that word in string"""
     symbols = ['!', '(', ')', ',', '.']
     for symbol in symbols:
         str = str.replace(symbol, '')
 
-    dict = {
-        word: 0
-    }
-
     str_split = str.split(" ")
+    str_set = set(str_split)
 
-    for s in str_split:
-        if s == word:
-            value = dict[word]
-            dict[word] = value + 1
+    for s in str_set:
+        dict = {
+            s: 0
+        }
 
-    return dict[word]
+    for s in str_set:
+        count = 0
+        for word in str_split:
+            if (s == word):
+                count = count + 1
+        dict[s] = count
+
+    return dict
 
 
 def main():
     str = input("Enter a string: ")
-    word = input("Enter word to search: ")
-    frequency = getFrequency(str, word)
-    print(frequency)
+    while True:
+        word = input("Enter word to search(quit() to quit): ")
+        if(word == "quit"):
+            print("Exiting...")
+            exit(1)
+        str_dict = getFrequency(str)
+        print(f"Frequency of {word} is : {str_dict[word]}")
 
 
 if __name__ == '__main__':
